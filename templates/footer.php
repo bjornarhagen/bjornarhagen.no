129608d82f8f1c5dcabe8442ea26e72d2aabe197
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-08-14 02:29:09 +0200

    Updated typography

--- templates/footer.php
+++ templates/footer.php
@@ -3,4 +3,5 @@
 		<p>(C) Copyright <?= date("Y"); ?> - Bjørnar Hagen</p>
 	</div>
 </footer>
+<link href="https://cdn.datahjelpen.no/fonts/butler/butler-700.css" rel="stylesheet">
 <?php require_once('scripts.php') ?>
