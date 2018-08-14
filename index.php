3e23a2ac9a6c5729c517e20857ca571178a80c0d
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-08-14 02:33:55 +0200

    Updated front-page typography

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <?php require_once('templates/nav-index.php'); ?>
     <header class="wrapper">
         <div class="wrapper-inner">
-            <h3>Front-end web designer</h3>
+            <h2>Front-end web designer</h2>
             <h1>Bjørnar Hagen</h1>
             <p>I'm a front-end web designer — Focused on creating minimal interfaces for web, desktop & mobile.</p>
             <p>Currently at <a href="https://datahjelpen.no" target="_blank">Datahjelpen AS</a>.</p>
