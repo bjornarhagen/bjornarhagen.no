3fb266ad9b539e23e3bc50d33ee4b965d6b8bf32
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-09-07 11:47:39 +0200

    Remove old files

--- templates/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="wrapper">
-	<div class="wrapper-inner">
-		<p>(C) Copyright <?= date("Y"); ?> - Bjørnar Hagen</p>
-	</div>
-</footer>
-<link href="https://cdn.datahjelpen.no/fonts/butler/butler-700.css" rel="stylesheet">
-<?php require_once('scripts.php') ?>
