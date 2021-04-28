13ee2d9798bd62d4d25f7e62a0c123b87c915ce2
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-04-28 22:41:22 +0200

    Update the JSON post editor

--- src/pages/_/editor/editor.js
+++ src/pages/_/editor/editor.js
@@ -38,25 +38,58 @@ function setupEditorWrapper(editorWrapper) {
   }
   let jsonTemplate = {}
 
-  const editorForm = document.createElement('form')
   const editorInput = document.createElement('textarea')
   editorInput.name = 'editor-input'
   editorInput.classList.add('editor-input')
+
+  const editorInputPreview = document.createElement('div')
+  editorInputPreview.classList.add('editor-input-preview')
+  editorInputPreview.classList.add('text-content')
+
+  const editorInputWrapper = document.createElement('div')
+  editorInputWrapper.classList.add('editor-input-wrapper')
+  editorInputWrapper.appendChild(editorInput)
+  editorInputWrapper.appendChild(editorInputPreview)
+
   const editorArea = document.createElement('div')
   editorArea.classList.add('editor-area')
+
   const editorOutput = document.createElement('textarea')
   editorOutput.name = 'editor-output'
   editorOutput.classList.add('editor-output')
-  //   editorOutput.readOnly = true
+  editorOutput.style.display = 'none'
+
+  const editorMessageArea = document.createElement('p')
+  editorMessageArea.classList.add('editor-message-area')
+  editorMessageArea.style.display = 'none'
+
   const editorSaveButton = document.createElement('button')
   editorSaveButton.type = 'submit'
   editorSaveButton.appendChild(document.createTextNode('Save'))
 
+  const editorInputButton = document.createElement('button')
+  editorInputButton.type = 'button'
+  editorInputButton.appendChild(document.createTextNode('Input JSON'))
+  editorInputButton.classList.add('secondary')
+
+  const editorForm = document.createElement('form')
   editorForm.appendChild(editorArea)
-  editorForm.appendChild(editorInput)
+  editorForm.appendChild(editorInputWrapper)
+  editorForm.appendChild(editorMessageArea)
   editorForm.appendChild(editorSaveButton)
+  editorForm.appendChild(editorInputButton)
   editorForm.appendChild(editorOutput)
   editorWrapper.appendChild(editorForm)
+
+  let oldInputLength = editorInput.value.length
+  if (!!marked) {
+    setInterval(() => {
+      if (!!editorInput.value && editorInput.value.length !== oldInputLength) {
+        editorInputPreview.innerHTML = marked(editorInput.value)
+        oldInputLength = editorInput.value.length
+      }
+    }, 500)
+  }
 
   getTemplatePath(editorWrapper).then((templatePath) => {
     console.log(templatePath)
@@ -77,6 +110,7 @@ function setupEditorWrapper(editorWrapper) {
     jsonOutput['meta'] = jsonTemplate
     formInputEventCallback({ target: editorInput })
     copyToClipboard(editorOutput.value)
+    displayEditorMessage(editorMessageArea, editorSaveButton, 'JSON copied to clipboard!')
     jsonOutput['meta'] = null
     formInputEventCallback({ target: editorInput })
   })
@@ -102,12 +136,37 @@ function setupEditorWrapper(editorWrapper) {
       }
 
       editorInput.value = jsonOutput['content'][0].text
+      editorArea.style.display = ''
+      editorInputWrapper.style.display = ''
+      editorSaveButton.style.display = ''
+      editorOutput.style.display = 'none'
     } else if (input.name) {
       jsonOutput[input.name] = input.value
     }
 
     editorOutput.value = JSON.stringify(jsonOutput, null, '  ')
   }
+
+  // Show JSON input
+  editorInputButton.addEventListener('click', function () {
+    const originalEditorInputButtonText = editorInputButton.innerText
+    editorInputButton.innerText = 'Paste JSON in the textarea'
+    editorInputButton.disabled = true
+
+    editorArea.style.display = 'none'
+    editorInputWrapper.style.display = 'none'
+    editorSaveButton.style.display = 'none'
+    editorOutput.style.display = ''
+
+    setTimeout(() => {
+      editorInputButton.innerText = originalEditorInputButtonText
+      editorInput.style.display = ''
+      editorArea.style.display = ''
+      editorSaveButton.style.display = ''
+      editorOutput.style.display = 'none'
+      editorInputButton.disabled = false
+    }, 10000)
+  })
 }
 
 function setupEditorInputs(editorInput, editorOutput, editorArea, jsonTemplate) {
@@ -148,4 +207,18 @@ function copyToClipboard(text) {
       document.body.removeChild(textarea)
     }
   }
+}
+
+function displayEditorMessage(editorMessageArea, editorSaveButton, message) {
+  editorMessageArea.innerText = message
+  editorMessageArea.style.display = ''
+
+  editorSaveButton.style.display = 'none'
+
+  setTimeout(() => {
+    editorMessageArea.style.display = 'none'
+    editorMessageArea.innerText = ''
+
+    editorSaveButton.style.display = ''
+  }, 3000)
 }
