74c03dbcc6867fafa158b690a47248ad91e105e6
bjornarhagen <post@bjornarhagen.no>
2024-09-02 14:43:10 +0200

    fix: make run.sh script handle podman/docker correctly

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-# Function to check if a command exists
+# Check if a command exists
 command_exists() {
-    type "$1" &>/dev/null
+    command -v "$1" >/dev/null 2>&1
 }
 
 # Check for podman and docker
 if command_exists podman; then
+    echo "Using podman as the container tool"
     CONTAINER_TOOL=podman
 elif command_exists docker; then
+    echo "Using docker as the container tool"
     CONTAINER_TOOL=docker
 else
     echo "Could not find podman or docker. Please install one of them and try again."
@@ -16,4 +18,4 @@ else
 fi
 
 # Run the command
-$CONTAINER_TOOL run --rm -it -v $(pwd):/src -p 1313:1313 docker.io/klakegg/hugo:0.101.0-ext server
+$CONTAINER_TOOL run --rm -it -v "`pwd`:/src" -p 1313:1313 docker.io/klakegg/hugo:0.101.0-ext server
